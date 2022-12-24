41bdd486d04762d333c7d353c80c3b22b2e6b4fe
Sergio Drajner <sergio.drajner@gmail.com>
2022-12-24 18:49:25 +0200

    Add test_division to test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -15,3 +15,6 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+	assert 14 == calculator.divide(28, 2)
